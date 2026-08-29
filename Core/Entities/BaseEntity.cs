58e1bb8462482db850c1cb934d460f9c56381166
Sihle Mthembu <innocentsihle29@gmail.com>
2026-08-29 19:07:52 +0200

    Start-up

---
+++ Core/Entities/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Entities;
+
+public class BaseEntity
+{
+    public int Id { get; set; }
+}
---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,14 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Data;
+
+public class StoreContext : DbContext
+{
+    public StoreContext(DbContextOptions options) : base(options) {}
+
+    public DbSet<Product> Products { get; set; }
+}
