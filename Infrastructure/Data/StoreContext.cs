fefb9eb9554fb74171c604235072d736d6fa66ca
Sihle Mthembu <innocentsihle29@gmail.com>
2026-08-30 08:44:40 +0200

    Configured the DB

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,4 +1,5 @@
 ﻿using Core.Entities;
+using Infrastructure.Config;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -11,4 +12,11 @@ public class StoreContext : DbContext
     public StoreContext(DbContextOptions options) : base(options) {}
 
     public DbSet<Product> Products { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ProductConfiguration).Assembly);
+    }
 }
